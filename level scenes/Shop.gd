0a6aca1d4b1bcd9595826e2c2bff26f7fab48fc5
Bush <159691515+Bush-U@users.noreply.github.com>
2024-04-07 18:17:12 +0100

    hide shop when you go in
    area 3d still buggy asf

--- level scenes/Shop.gd
+++ level scenes/Shop.gd
@@ -1,29 +1,39 @@
 extends Node3D
 
-
+var inShop = false
+var weirdBugfix = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	inShop = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	print(inShop)
+	if inShop == false: #if we're in the shop, hide the roof
+		get_node("Roof").visible = true
+	else:
+		get_node("Roof").visible = false
 
 
-func _on_player_entered(body):
+func _on_player_entered(CharacterBody3D):
+	inShop = true
 	print("Player entered!")
-	change_roof_opacity(0.7)
+	
+	#change_roof_opacity(0.7)
 
 
-func _on_area_3d_body_exited(body):
+func _on_area_3d_body_exited(CharacterBody3D):
+	inShop = false
 	print("Player exited!")
-	change_roof_opacity(0)
+	
+	#change_roof_opacity(0)
 	
 	
-func change_roof_opacity(opacity):
+func change_roof_opacity(opacity): #this doesn't work atm
 	#get all the nodes that make up the roof and set the opacity to them
-	var roof_parts = ["Roof/Back Wall2/wall/MeshInstance3D", "snalalp", "Roof/floor2/wall/MeshInstance3D", "Roof/Left wall2/wall/MeshInstance3D"]
+	var roof_parts = ["Roof/Back Wall2/wall/MeshInstance3D", "Roof/Right Wall2/wall/MeshInstance3D", "Roof/floor2/wall/MeshInstance3D", "Roof/Left wall2/wall/MeshInstance3D"]
 	for part_path in roof_parts:
 		var part = get_node_or_null(part_path)
 		if part and part.mesh and part.mesh.surface_get_material(0):
