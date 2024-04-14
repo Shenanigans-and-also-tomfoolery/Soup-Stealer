d31a55600a79788beefffabf2e33cb4b66e6ddb1
Bush <159691515+Bush-U@users.noreply.github.com>
2024-04-14 15:55:59 +0100

    Added a topless guy as the player instead of using beanos
    added animation for each movement apart from jump bc idk if we're gonna use it or not

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const SPRINT_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
-const SPRINT_DURATION = 2.0  # Adjust the duration as needed
-const SPRINT_COOLDOWN = 5.0  # Adjust the cooldown time as needed
-var sprint_timer = 0.0
-var sprint_cooldown_timer = 0.0
 var is_sprinting = false
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -15,19 +12,9 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 
-	if sprint_cooldown_timer > 0:
-		sprint_cooldown_timer -= delta
-	else:
-		if Input.is_action_pressed("sprint") and sprint_timer <= SPRINT_DURATION:
-			is_sprinting = true
-			sprint_timer += delta
-			if sprint_timer > SPRINT_DURATION:
-				sprint_timer = SPRINT_DURATION
-		else:
-			is_sprinting = false
-			sprint_timer = 0.0
-			if sprint_cooldown_timer <= 0.0:
-				sprint_cooldown_timer = SPRINT_COOLDOWN
+	get_node("topless_guy").rotation.y = atan2(velocity.x,velocity.z) # Rotate the character's mesh based on the direction
+
+
 				
 	# Add the gravity.
 	if not is_on_floor():
@@ -44,13 +31,19 @@ func _physics_process(delta):
 	if direction:
 		if Input.is_action_pressed("sprint"):
 			velocity.x = direction.x * SPRINT_SPEED
-			velocity.z = direction.z * SPRINT_SPEED
+			velocity.z = direction.z * SPRINT_SPEED #We times by sprint speed instead of normal speed if player is press shift
 			is_sprinting = true
+			get_node("topless_guy/AnimationPlayer").play("Run")
 		else:
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
+			get_node("topless_guy/AnimationPlayer").play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		get_node("topless_guy/AnimationPlayer").play("Idle")
+		get_node("topless_guy").rotation.y = -160
+		
+		
 
 	move_and_slide()
