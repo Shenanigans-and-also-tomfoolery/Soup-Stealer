3eb969d8bf1928edefea93651aac85ea2fbbc9de
Bush <159691515+Bush-U@users.noreply.github.com>
2024-04-16 19:32:19 +0100

    Added low quality title screen v1
    + some other small stuff

---
+++ boot_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("AnimationPlayer").play("boot_animation")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://title_screen.tscn")
+	pass # Replace with function body.
